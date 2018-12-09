76e9b624bd19b26c17163eeb407063176def621c
Sebastien <sebast.lubineau@laposte.net>
2018-12-09 10:52:37 +0100

    fix(app): rename import

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,5 @@
 export * from './store/store.interface';
 export * from './store/smart-objects-state';
-export * from './smartobject';
+export * from './smart-object';
 export * from './user';
 export * from './store/user-state';
