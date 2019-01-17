cc81f470cc6db4642510af6b3b5c378154e76b19
Daniel Taylor <danny.taylor@live.fr>
2019-01-17 11:00:10 +0100

    change component index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './ObjectCard/ObjectCard';
 export {default as Notification} from "./Notification/Notification";
 export {default as Header} from './Header/Header';
 export {default as Selector} from './Selector/Selector';
+export {ScenarioCondition} from './ScenarioCondition/ScenarioCondition';
